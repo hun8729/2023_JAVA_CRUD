f4ad30bb3eb188acfdfa6b86b998e1bdaaaaa0ae
hun8729 <hun8729@handong.ac.kr>
2023-09-04 20:47:35 +0900

    Implement Main.java

---
+++ src/com/mycom/word/Main.java
@@ -0,0 +1,8 @@
+package com.mycom.word;
+
+public class Main {
+    public static void main(String[] args){
+        WordManager wordManager = new WordManager();
+        wordManager.start();
+    }
+}
